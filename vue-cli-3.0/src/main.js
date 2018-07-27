e614c52df44ee59db1254eb9253696445e55cce0
steven <bluedwy@126.com>
2018-07-27 11:24:57 +0800

    引入 vue-antd-ui
    引入vue antd ui 框架 并且写入一个测试页面

--- vue-cli-3.0/src/main.js
+++ vue-cli-3.0/src/main.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Antd from "vue-antd-ui";
 import router from "./router";
 import store from "./store";
+import "vue-antd-ui/dist/antd.css";
 
 Vue.config.productionTip = false;
+Vue.use(Antd);
 
 new Vue({
   router,
